4b032300b95d3b1d66bfc2b6ec6ad32673b1c41b
wadi-1000 <joy.kiranga@student.moringaschool@gmail.com>
2021-08-06 14:29:11 +0300

    Generated API Key and stored it in a folder secret

--- app/views.py
+++ app/views.py
@@ -8,5 +8,5 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-
-    return render_template('index.html')
+    title = 'News Dr.'
+    return render_template('index.html', title = title)
