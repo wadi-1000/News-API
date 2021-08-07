83ad6712b1e87f2a9ced47b32996956a60dee82c
wadi-1000 <joy.kiranga@student.moringaschool@gmail.com>
2021-08-07 16:21:40 +0300

    Created and instantiated Articles Class to hold all article objects

---
+++ app/models/articles.py
@@ -0,0 +1,14 @@
+class Articles:
+    '''
+    Articles Class to define Article objects
+    '''
+
+     def __init__(self,author,title,description,url,urlToImage,publishedAt):
+         self.author = author
+         self.title = title
+         self.description = description
+         self.url = url
+         self.urlToImage = urlToImage
+         self.publishedAt = publishedAt
+
+        
