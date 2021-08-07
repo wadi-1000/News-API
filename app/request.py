d5f1ece178eea878def6ee6a1aacfb69b8a2a927
wadi-1000 <joy.kiranga@student.moringaschool@gmail.com>
2021-08-07 12:43:35 +0300

    Created function to process the json results into a list

--- app/request.py
+++ app/request.py
@@ -17,7 +17,7 @@ def get_news(sources):
     '''
     Function that gets json response to our url request
     '''
-    get_news_url = base_url.format(source, api_key)
+    get_news_url = base_url.format(sources, api_key)
 
     with urllib.request.urlopen(get_news_url) as url:
         get_news_data = url.read()
@@ -25,8 +25,36 @@ def get_news(sources):
 
         news_results = None
 
-        if get_news_response['source']:
-            news_results_list = get_news_response['source']
+        if get_news_response['sources']:
+            news_results_list = get_news_response['sources']
             news_results = process_results(news_results_list)
+
+    return news_results
+
+
+def process_results(news_list)
+    '''
+    Function that processes the news result and transform them to a list of objects
+
+    Args:
+        news_list: A list of dictionaries that contain movie details
+
+    Returns :
+        news_results: A list of news objects
+    '''
+    news_results =[]
+    for news_item in news_list:
+        id = news_item.get('id')
+        name = news_item.get('name')
+        description = news_item.get('description')
+        url = news_item.get('url')
+        category = news_item.get('category')
+        language = news_item.get('language')
+        country = news_item.get('country')
+        urlsToLogos = news_item.get('small')
+
+        if urlsToLogos:
+            news_object = Sources(id,name,description,url,category,language,country,urlsToLogos)
+            news_results.append(news_object)
 
     return news_results
