e008d24ad65f3f5c472fceec24bf68ff7079b051
wadi-1000 <joy.kiranga@student.moringaschool@gmail.com>
2021-08-07 12:13:35 +0300

    Made first api call to get news sources

--- app/request.py
+++ app/request.py
@@ -1,4 +1,32 @@
 from app import app
+from urllib.request,json
+from .models import sources
+
+Sources = sources.Sources
+
 
 # Getting api key
 api_key = app.config['NEWS_API_KEY']
+
+
+# Getting base url
+base_url = app.config["NEWS_API_BASE_URL"]
+
+
+def get_news(sources):
+    '''
+    Function that gets json response to our url request
+    '''
+    get_news_url = base_url.format(source, api_key)
+
+    with urllib.request.urlopen(get_news_url) as url:
+        get_news_data = url.read()
+        get_news_response = json.loads(get_news_data)
+
+        news_results = None
+
+        if get_news_response['source']:
+            news_results_list = get_news_response['source']
+            news_results = process_results(news_results_list)
+
+    return news_results
