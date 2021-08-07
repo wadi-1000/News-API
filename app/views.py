1551ad011707d15d323571117df2d978c442f33a
wadi-1000 <joy.kiranga@student.moringaschool@gmail.com>
2021-08-07 12:47:18 +0300

    Added sources variable to render_template to display sources

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from app import app
+from .request import get_news
 
 
 #Views
@@ -8,5 +9,9 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
+
+    # Getting news sources
+    news_sources = get_news('sources')
+    print(news_sources)
     title = 'News Dr.'
-    return render_template('index.html', title = title)
+    return render_template('index.html', title = title, sources = news_sources)
